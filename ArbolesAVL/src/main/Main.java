82a3d4b06deebf5873c7a3d350100777244468b5
Josue <josuenunezprada@gmail.com>
2020-04-28 16:45:05 -0500

    Avance interfaz

--- ArbolesAVL/src/main/Main.java
+++ ArbolesAVL/src/main/Main.java
@@ -1,10 +1,13 @@
 package main;
 
+import vista.VistaPrincipal;
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		VistaPrincipal vPrin = new VistaPrincipal();
+		vPrin.iniciar();
 	}
 
 }
