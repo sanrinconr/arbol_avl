2459c2f5841a5e2e0cf83e77650d2d639ee11be4
Santiago Rincon <santiagorincon2001@gmail.com>
2020-04-28 21:19:36 -0500

    Se agregan comentarios

--- AVL/src/launcher/Main.java
+++ AVL/src/launcher/Main.java
@@ -31,14 +31,35 @@ public class Main {
             System.out.print(h+",");
         }
         
-        /*if(arbol.RetornarRaiz()!=null){
-        Nodo dato =arbol.RetornarRaiz();
-        System.out.println(dato.llave);
-        System.out.println(dato.balance);
-        }*/
+      
+        //TODO ESTO HACE VER LA VENTANA BONITA QUITAR PARA PROBAR COMO SE VE COMO SE VE NORMAL
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //HASTA AQUI ES LO BONITO, NI IDEA QUE SEA LO QUE HACE
         
+        //EN LA LINEA 202 metodo botonInsertarActionPerformed de VistaPrincipal.java esta lo que hice para acoplarlo con la version consola
+       
         VistaPrincipal ventana = new VistaPrincipal();
-        //ventana.iniciar();
+        ventana.setVisible(true);
+        
+        //HAY UN PROBLEMA Y ES QUE A VECES ACEPTA UN NUMERO REPETIDO
+        //Esas clases Simulador ArbolExpresion.. de vista son logica de la vista, no supe donde mas ponerla
+        //Lo que se hace es crear el arbol de la version consola, obtener la impresion por niveles, eso pasarselo a la logica de la grafica
+        //a su propia clase arbol y luego graficarla, es como un puente
         
     }
 }
