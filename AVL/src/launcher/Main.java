c8e4223ca0ff614d09de87899e5a3caa4e604905
Santiago Rincon <santiagorincon2001@gmail.com>
2020-05-03 13:40:02 -0500

    Limpieza main v1.1

--- AVL/src/launcher/Main.java
+++ AVL/src/launcher/Main.java
@@ -17,31 +17,32 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-      
-        //TODO ESTO HACE VER LA VENTANA BONITA QUITAR PARA PROBAR COMO SE VE COMO SE VE NORMAL
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //HASTA AQUI ES LO BONITO, NI IDEA QUE SEA LO QUE HACE
+              
+       //EN LA LINEA 202 metodo botonInsertarActionPerformed de VistaPrincipal.java esta lo que hice para acoplarlo con la version consola
+       detalles();
+       VistaPrincipal ventana = new VistaPrincipal();
+       ventana.setVisible(true);
         
-        //EN LA LINEA 202 metodo botonInsertarActionPerformed de VistaPrincipal.java esta lo que hice para acoplarlo con la version consola
-       
-        VistaPrincipal ventana = new VistaPrincipal();
-        ventana.setVisible(true);
         
          
+    }
+    public static void detalles() {
+    	  //TODO ESTO HACE VER LA VENTANA BONITA QUITAR PARA PROBAR COMO SE VE COMO SE VE NORMAL
+    	 try {
+             for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                 if ("Nimbus".equals(info.getName())) {
+                     javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                     break;
+                 }
+             }
+         } catch (ClassNotFoundException ex) {
+             java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         } catch (InstantiationException ex) {
+             java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         } catch (IllegalAccessException ex) {
+             java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+             java.util.logging.Logger.getLogger(VistaPrincipal.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+         }
     }
 }
